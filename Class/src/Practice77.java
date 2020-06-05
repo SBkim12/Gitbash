e418dbe60730accffb00494d500f9e0ba2057f80
SBkim12 <mar5924@naver.com>
2020-06-05 13:19:03 +0900

    첫경험

---
+++ Class/src/Practice77.java
@@ -0,0 +1,18 @@
+
+public class Practice77 {
+
+	public static void main(String[] args) {
+		int i;
+		
+		for(i =1; i<=100; i++) {
+			if(i==57) {
+			System.out.printf("for 문을  %d 회 실행했습니다. \n",i);
+			break;
+			}
+		}
+		
+		System.out.println("for 문을 종료했습니다.");
+
+	}
+
+}
---
+++ Class/src/Practice81.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Practice81 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a, b,c,d;
+		int hap;
+		
+		
+		System.out.print("1번째 숫자를 입력하세요 : ");
+		a = sc.nextInt();
+		System.out.print("2번째 숫자를 입력하세요 : ");
+		b = sc.nextInt();
+		System.out.print("3번째 숫자를 입력하세요 : ");
+		c = sc.nextInt();
+		System.out.print("4번째 숫자를 입력하세요 : ");
+		d = sc.nextInt();
+		
+		hap = a + b+ c+d;
+		
+		System.out.printf(" 합계 ==> %d \n", hap);
+	}
+
+}
